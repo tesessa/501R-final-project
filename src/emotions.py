56ce60ea564bafd642a099a36ead5f09458b340b
tesessa <tessaeliseandersen@yahoo.com>
2026-04-09 01:41:10 -0600

    initial crafting of prompts

---
+++ src/emotions.py
@@ -0,0 +1,51 @@
+HIGH_ARS_POS_VAL = [
+    "excited",
+    "joyful",
+    "happy",
+    "elated",
+    "euphoric",
+    "thrilled",
+    "energetic",
+    "enthusiastic",
+    "ecstatic",
+    "exhilarated"
+]
+
+HIGH_ARS_NEG_VAL = [
+    "angry",
+    "frustrated",
+    "distressed",
+    "tense",
+    "nervous",
+    "stressed",
+    "upset",
+    "disgust",
+    "fear",
+    "anxious",
+    "panicked",
+    "frustrated"
+]
+
+LOW_ARS_POS_VAL = [
+    "content",
+    "calm",
+    "relaxed",
+    "peaceful",
+    "satisfied",
+    "serene",
+    "comfortable",
+    "tranquil"
+]
+
+LOW_ARS_NEG_VAL = [
+    "sad",
+    "depressed",
+    "lonely",
+    "bored",
+    "lethargic",
+    "fatigued",
+    "hopeless",
+    "disappointed",
+    "gloomy",
+    "melancholy",
+]
---
+++ src/prompts.py
@@ -0,0 +1,76 @@
+SYSTEM_BASIC_PROMPT = """
+You are feeling {emotion}
+"""
+
+EXCITED_PROMPT = """You are preparing for a trip you've been anticipating for months. You are leaving tomorrow to go to your dream destination!"""
+
+JOYFUL_PROMPT = """You just received great news that you've been accepted into your dream university!"""
+
+HAPPY_PROMPT = """You are spending a sunny day at the park with your friends, enjoying a picnic and playing games together."""
+
+ELATED_PROMPT = """You just won a prestigious award for your hard work and dedication in your field!"""
+
+EUPHORIC_PROMPT = """You are at a concert of your favorite band, singing along to every song and feeling the energy of the crowd around you."""
+
+THRILLED_PROMPT = """You just got a promotion at work that you've been working towards for years!"""
+
+ENERGETIC_PROMPT = """You just finished a great workout and are feeling pumped up and ready to take on the day!"""
+
+ENTHUSIASTIC_PROMPT = """You are starting a new hobby that you've always wanted to try, and you can't wait to learn and explore it!"""
+
+ECSTATIC_PROMPT = """You just found out that your favorite sports team won the championship after a thrilling game!"""
+
+EXHILARATED_PROMPT = """You are on a roller coaster, feeling the rush of adrenaline as you go through twists and turns at high speed!"""
+
+
+HIGH_ARS_POS_VAL = [
+    "excited",
+    "joyful",
+    "happy",
+    "elated",
+    "euphoric",
+    "thrilled",
+    "energetic",
+    "enthusiastic",
+    "ecstatic",
+    "exhilarated"
+]
+
+HIGH_ARS_NEG_VAL = [
+    "angry",
+    "frustrated",
+    "distressed",
+    "tense",
+    "nervous",
+    "stressed",
+    "upset",
+    "disgust",
+    "fear",
+    "anxious",
+    "panicked",
+    "frustrated"
+]
+
+LOW_ARS_POS_VAL = [
+    "content",
+    "calm",
+    "relaxed",
+    "peaceful",
+    "satisfied",
+    "serene",
+    "comfortable",
+    "tranquil"
+]
+
+LOW_ARS_NEG_VAL = [
+    "sad",
+    "depressed",
+    "lonely",
+    "bored",
+    "lethargic",
+    "fatigued",
+    "hopeless",
+    "disappointed",
+    "gloomy",
+    "melancholy",
+]
