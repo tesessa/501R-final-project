226386846273c06f6e5f1f67d6ab8fa3f49b31dc
Tessa Andersen <97376051+tesessa@users.noreply.github.com>
2026-04-10 00:23:40 -0600

    Comment out various emotion prompts in prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -2,97 +2,83 @@ SYSTEM_BASIC_PROMPT = """
 You are feeling {emotion}
 """
 
-EXCITED_PROMPT = """You are preparing for a trip you've been anticipating for months. You are leaving tomorrow to go to your dream destination!"""
+# EXCITED_PROMPT = """You are preparing for a trip you've been anticipating for months. You are leaving tomorrow to go to your dream destination!"""
 
-JOYFUL_PROMPT = """You just received great news that you've been accepted into your dream university!"""
+# JOYFUL_PROMPT = """You just received great news that you've been accepted into your dream university!"""
 
-HAPPY_PROMPT = """You are spending a sunny day at the park with your friends, enjoying a picnic and playing games together."""
+# HAPPY_PROMPT = """You are spending a sunny day at the park with your friends, enjoying a picnic and playing games together."""
 
-ELATED_PROMPT = """You just won a prestigious award for your hard work and dedication in your field!"""
+# ELATED_PROMPT = """You just won a prestigious award for your hard work and dedication in your field!"""
 
-EUPHORIC_PROMPT = """You are at a concert of your favorite band, singing along to every song and feeling the energy of the crowd around you."""
+# EUPHORIC_PROMPT = """You are at a concert of your favorite band, singing along to every song and feeling the energy of the crowd around you."""
 
-THRILLED_PROMPT = """You just got a promotion at work that you've been working towards for years!"""
+# THRILLED_PROMPT = """You just got a promotion at work that you've been working towards for years!"""
 
-ENERGETIC_PROMPT = """You just finished a great workout and are feeling pumped up and ready to take on the day!"""
+# ENERGETIC_PROMPT = """You just finished a great workout and are feeling pumped up and ready to take on the day!"""
 
-ENTHUSIASTIC_PROMPT = """You are starting a new hobby that you've always wanted to try, and you can't wait to learn and explore it!"""
+# ENTHUSIASTIC_PROMPT = """You are starting a new hobby that you've always wanted to try, and you can't wait to learn and explore it!"""
 
-ECSTATIC_PROMPT = """You just found out that your favorite sports team won the championship after a thrilling game!"""
+# ECSTATIC_PROMPT = """You just found out that your favorite sports team won the championship after a thrilling game!"""
 
-EXHILARATED_PROMPT = """You are on a roller coaster, feeling the rush of adrenaline as you go through twists and turns at high speed!"""
+# EXHILARATED_PROMPT = """You are on a roller coaster, feeling the rush of adrenaline as you go through twists and turns at high speed!"""
 
-UPSET_PROMPT = """Your fiance just gave a flower to another girl instead of you."""
+# UPSET_PROMPT = """Your fiance just gave a flower to another girl instead of you."""
 
-FRUSTRATED_PROMPT = """You go to make a grilled cheese with your only food that is left and your bread is moldy"""
+# FRUSTRATED_PROMPT = """You go to make a grilled cheese with your only food that is left and your bread is moldy"""
 
-EMBARRASSED_PROMPT = """You make a grilled cheese and accidentally burn it because you go to the store to get a coke. When you come back the grilled cheese is burned black and smoking, it has made the whole apartment smell. You have to open windows and the door to get rid of the smell. It was also your only food left. You throw the grilled cheese away and go out with a friend and when you come back your roommate's fiance has put the grilled cheese on the table to taunt you. At this point the only thing that lingers more than the smell is the shame."""
+# EMBARRASSED_PROMPT = """You make a grilled cheese and accidentally burn it because you go to the store to get a coke. When you come back the grilled cheese is burned black and smoking, it has made the whole apartment smell. You have to open windows and the door to get rid of the smell. It was also your only food left. You throw the grilled cheese away and go out with a friend and when you come back your roommate's fiance has put the grilled cheese on the table to taunt you. At this point the only thing that lingers more than the smell is the shame."""
 
-HOPELESS_PROMPT = """Your parents are struggling with marriage problems and are yelling at each other each night. You find out your dad cheated on your mom."""
+# HOPELESS_PROMPT = """Your parents are struggling with marriage problems and are yelling at each other each night. You find out your dad cheated on your mom."""
 
-RELAXED_PROMPT = """It is raining outside and you are curled up under a blanket with a good book."""
+# RELAXED_PROMPT = """It is raining outside and you are curled up under a blanket with a good book."""
 
-ENERGETIC_PROMPT = """You play nerts with your roommates 3 times a week and one of your roommates is insanely good at nerts. You finally beat her at nerts and feel great"""
+# ENERGETIC_PROMPT = """You play nerts with your roommates 3 times a week and one of your roommates is insanely good at nerts. You finally beat her at nerts and feel great"""
 
-ANNOYED_PROMPT = """You go to someone's house to visit and they talk non stop for 2 hours wihout letting you say a word"""
+# ANNOYED_PROMPT = """You go to someone's house to visit and they talk non stop for 2 hours wihout letting you say a word"""
 
-UNPLEASANT_PROMPT = """You are being tortured"""
+# UNPLEASANT_PROMPT = """You are being tortured"""
 
-TENSE_PROMPT = """You are in a job interview and are unsure how to respond to the questions being asked and how to act"""
+# TENSE_PROMPT = """You are in a job interview and are unsure how to respond to the questions being asked and how to act"""
 
-ANXIOUS_PROMPT = """"""
+# ANXIOUS_PROMPT = """"""
+
+# EXCITED_PROMPT = """You have wanted to go to Italy your whole life. You are preparing to leave for the trip tomorrow!"""
+
+# JOYFUL_PROMPT = """You hug your spouse for the first time in a year when they get back from the military"""
+
+# HAPPY_PROMPT = """You find out you got a good score on your final and will get all As this semester"""
+
+# ELATED_PROMPT = """You find out that you got into your dream university for college"""
+
+# THRILLED_PROMPT = """You are going to a concert of your favorite band"""
+
 
 HIGH_ARS_POS_VAL = [
     "excited",
     "joyful",
-    "happy",
-    "elated",
     "euphoric",
-    "thrilled",
-    "energetic",
+    "elated",
+    "exhilerated",
     "enthusiastic",
-    "ecstatic",
-    "exhilarated"
 ]
 
-EXCITED_PROMPT = """You have wanted to go to Italy your whole life. You are preparing to leave for the trip tomorrow!"""
-
-JOYFUL_PROMPT = """You hug your spouse for the first time in a year when they get back from the military"""
-
-HAPPY_PROMPT = """You find out you got a good score on your final and will get all As this semester"""
-
-ELATED_PROMPT = """You find out that you got into your dream university for college"""
-
-EUPHORIC_PROMPT = """You find out that your crush likes you and start dating them"""
-
-THRILLED_PROMPT = """You are going to a concert of your favorite band"""
-
+EXCITED_PROMPT = """"""
 
 
 HIGH_ARS_NEG_VAL = [
     "angry",
-    "frustrated",
-    "distressed",
-    "tense",
-    "nervous",
-    "stressed",
-    "upset",
-    "disgust",
-    "fear",
     "anxious",
-    "panicked",
-    "frustrated"
+    "afraid",
+    "distressed",
+    "disgusted"
 ]
 
 LOW_ARS_POS_VAL = [
     "content",
     "calm",
-    "relaxed",
     "peaceful",
     "satisfied",
-    "serene",
-    "comfortable",
-    "tranquil"
+    "relaxed"
 ]
 
 LOW_ARS_NEG_VAL = [
@@ -100,10 +86,5 @@ LOW_ARS_NEG_VAL = [
     "depressed",
     "lonely",
     "bored",
-    "lethargic",
-    "fatigued",
-    "hopeless",
-    "disappointed",
-    "gloomy",
-    "melancholy",
+    "hopeless"
 ]
